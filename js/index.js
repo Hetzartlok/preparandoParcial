a9e97b71ff41422e9ebf5c844925aad41f58f105
hetzartlok <Laboratorio@SBAWSD02LC816.duoc.acad>
2024-05-17 17:53:05 -0400

    Imagenes responsivas al mouseOver

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 // Add event listeners to the images to make them larger on click
 const images = document.querySelectorAll('img');
 images.forEach(image => {
-    image.addEventListener('click', () => {
-        image.style.width = '600px';
-        image.style.height = '502px';
-    });
+
+    image.addEventListener('mouseover', () =>{
+        image.style.width = '50%';
+        image.style.height = '50%';
+    })
+    image.addEventListener('mouseout', () => {
+        image.style.width = '30%';
+        image.style.height = '30%';
+    })
 });
