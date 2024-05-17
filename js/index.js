b0f38796bb8582f16b2409fc21fdf8175554c82e
hetzartlok <Laboratorio@SBAWSD02LC816.duoc.acad>
2024-05-17 17:40:50 -0400

    Commit de pagina basica para editar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-Este es un documento de js del index!
+// Add event listeners to the images to make them larger on click
+const images = document.querySelectorAll('img');
+images.forEach(image => {
+    image.addEventListener('click', () => {
+        image.style.width = '600px';
+        image.style.height = '502px';
+    });
+});
